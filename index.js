2d6e416991e46218582c24f54270c1f4dd240793
Richardy Tanure <richardy.tanure@hotmail.com>
2022-07-06 09:26:42 -0300

    CORS configuration

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 8080
 
 app.use(express.json())
 app.use(cors({
-    "origin": "http://localhost:3000/"
+    origin: "http://localhost:3000"
 }))
 
 var notesList = [
